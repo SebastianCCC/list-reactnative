d6bb282c3a17e6401881146f091fcece7695b3ad
SebastianCCC <89015968+SebastianCCC@users.noreply.github.com>
2022-12-21 23:01:13 +0100

    add ui && styles

--- App.tsx
+++ App.tsx
@@ -2,18 +2,37 @@
  * @format
  */
 
-import React, {useEffect} from 'react'
-import {SafeAreaView, StatusBar, useColorScheme} from 'react-native'
+import React, {useEffect, useState} from 'react'
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native'
 import supabase from './supabaseConfig'
 
 import {Colors} from 'react-native/Libraries/NewAppScreen'
+import List from './src/Components/List'
 
 const App = () => {
+  const [list, setList] = useState<any[] | null>([])
   const isDarkMode = useColorScheme() === 'dark'
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    flex: 1,
   }
+
+  const style = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#e6e6e6',
+      padding: 25,
+    },
+  })
 
   useEffect(() => {
     fetchTodos()
@@ -21,7 +40,7 @@ const App = () => {
 
   const fetchTodos = async () => {
     let {data: todos, error} = await supabase.from('todos').select('*')
-    console.log(todos)
+    setList(todos)
   }
 
   return (
@@ -30,6 +49,31 @@ const App = () => {
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         backgroundColor={backgroundStyle.backgroundColor}
       />
+      <View style={style.container}>
+        <Text
+          style={{
+            paddingBottom: 25,
+            fontSize: 18,
+            textTransform: 'uppercase',
+            textAlign: 'center',
+          }}>
+          Todos
+        </Text>
+        <ScrollView showsVerticalScrollIndicator={false}>
+          {list?.length
+            ? list!.map(({id, inserted_at, is_complete, task, user_id}) => (
+                <List
+                  key={id}
+                  id={id}
+                  inserted_at={inserted_at}
+                  is_complete={is_complete}
+                  task={task}
+                  user_id={user_id}
+                />
+              ))
+            : null}
+        </ScrollView>
+      </View>
     </SafeAreaView>
   )
 }
